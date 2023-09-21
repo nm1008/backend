db8d126af54c90944290d67bf3fe813878d52e05
nm1008 <mallari08nikko@yahoo.com>
2023-09-21 08:54:54 +0800

    added feature-server connecting to mongoDB database

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.get("/", async(req, res) => {
     console.log("get")
+    res.send("I am from node")
 })
 
 //PORT
@@ -14,3 +15,14 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 });
+
+//connecting to mongoDB database and listening to port (whatever that is stored in the .env or default 8000 and the server cannot connect to the server it will log the error message)
+mongoose
+  .connect(process.env.DB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+  })
+  .catch((err) => {
+    console.log(err);
+  });
