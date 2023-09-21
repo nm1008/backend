8fa9bf731e61dd4959dafcd0845d9fe75fd991bf
nm1008 <mallari08nikko@yahoo.com>
2023-09-21 20:49:18 +0800

    added controller/models/routes

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const app = express();
-
-app.get("/", async(req, res) => {
-    console.log("get")
-    res.send("I am from node")
-})
+const Course = require("./models/course");
 
 //PORT
 const PORT = process.env.PORT || 8000;
 
-
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
-});
+//MIDDLEWARE
+app.use(express.json());
 
 //connecting to mongoDB database and listening to port (whatever that is stored in the .env or default 8000 and the server cannot connect to the server it will log the error message)
 mongoose
   .connect(process.env.DB)
   .then(() => {
     console.log("Connected to MongoDB");
-
+    app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.log(err);
   });
+
+// routes
+const courseRoutes = require("./routes/course");
+app.use("/api/courses", courseRoutes);
