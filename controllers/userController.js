24dfe70b31ec333a3516134f14ed93086828ac1d
nm1008 <mallari08nikko@yahoo.com>
2023-09-25 07:02:51 +0800

    remodify code and implemented bcrypt on registerUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
 const User = require("../models/user");
 
+//bcrypt to hash passwords (check registerUser)
+const bcrypt = require("bcrypt");
+
 //get all user
 const getAllUser = async (req, res) => {
   try {
@@ -14,13 +17,34 @@ const getAllUser = async (req, res) => {
 //register a user
 const registerUser = async (req, res) => {
   try {
-    const registerUser = await User.create(req.body);
+    //bcrypt - generate salt and hash the user's password
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create a new user with the hashed password
+    let newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      mobileNo: req.body.mobileNo,
+      password: hashedPassword,
+      // or what we can do is bcrypt.hashSync(req.body.password, 10) // 10 is salt
+    });
+
+    //save the newUser to the DB
+    const registeredUser = await newUser.save();
+
+    //response
     res.status(200).json(registerUser);
+
+    // const registerUser = await User.create(req.body);
+    // res.status(200).json(registerUser);
   } catch (err) {
     console.log(err.message);
     res.status(500).json({ message: err.message });
   }
 };
+
 module.exports = {
   getAllUser,
   registerUser,
