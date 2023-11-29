33e6333def76baa6198bfb3a8aec84316c7dee16
nm1008 <mallari08nikko@yahoo.com>
2023-11-30 01:37:47 +0800

    updated server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://newback-simply-book.onrender.com"],
+    origin: ["https://simply-book-zgyr.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
